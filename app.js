570b4eb050d2313726e70b7c111700290c734ae5
mahesh <mahesh.hebballi@gmail.com>
2013-11-04 03:03:11 +0530

    Field projection

--- app.js
+++ app.js
@@ -22,13 +22,27 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
 	// });
 
 
-	var cursor = db.collection('grades').find(query);
-	cursor.each(function(err, doc){
+	// var cursor = db.collection('grades').find(query);
+	// //.each queries data
+	// cursor.each(function(err, doc){
+	// 	if(err) throw err;
+	// 	if(doc == null) {
+	// 		return db.close();
+	// 	}
+	// 	console.dir(doc.student + " got a good grade!");
+	// });
+
+	// Field Projection
+
+	var projection = {'student':1, '_id':0};
+	db.collection('grades').find(query, projection).toArray(function(err, docs){
 		if(err) throw err;
-		if(doc == null) {
-			return db.close();
-		}
-		console.dir(doc.student + " got a good grade!");
+
+		docs.forEach(function(doc){
+			console.dir(doc);
+			console.dir(doc.student + " got a good grade");
+		});
+		db.close();
 	});
 
 });

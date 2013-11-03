eeacb04d77d2bd3b071db3709030313c54c2e3c6
mahesh <mahesh.hebballi@gmail.com>
2013-11-04 02:44:24 +0530

    find and cursor

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
 
 	var query = {'grade' : 100};
 
-	db.collection('grades').findOne(query, function(err, doc){
+	//findOne
+
+	// db.collection('grades').findOne(query, function(err, doc){
+	// 	if(err) throw err;
+	// 	console.dir(doc);
+	// 	db.close();
+	// });
+
+	//find
+	//db.collection('grades').find(query) is the cursor
+	// db.collection('grades').find(query).toArray(function(err,docs){
+	// 	if(err) throw err;
+	// 	console.dir(docs);
+	// 	db.close();
+	// });
+
+
+	var cursor = db.collection('grades').find(query);
+	cursor.each(function(err, doc){
 		if(err) throw err;
-		console.dir(doc);
-		db.close();
+		if(doc == null) {
+			return db.close();
+		}
+		console.dir(doc.student + " got a good grade!");
 	});
+
 });
